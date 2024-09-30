fe72ad937fb5cd6ded0d373915e31fc3a427ab81
Amin Solkar <aminsolkar5@gmail.com>
2024-09-30 19:57:18 +0530

    Remove stock tracker

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router-dom";
 import Learn from "./Pages/Learn/Learn";
-import StockTracker from "./Pages/Tools/StockTracker";
+
 
 function App() {
   return (
