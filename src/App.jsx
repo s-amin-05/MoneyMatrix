1653bb9852defdadfeb13b120bc5c6142604af8f
Ayaan9421 <ayaanshaikh9421@gmail.com>
2024-09-30 14:29:10 +0530

    merging something

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import BudgetSim from "./Pages/Learn/BudgetSim";
-import EMICalculator from "./Pages/Tools/EMICalculator"
+
 function App() {
   return (
     <>
-      <EMICalculator />
+
     </>
   );
 }
